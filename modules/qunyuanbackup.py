2c5aba95fe3cdc7aceefaeaa3177cd77b0cf3142
zxxhz <ou0897@foxmail.com>
2024-07-29 22:58:30 +0800

    feat(qunyuanbackup.py): 添加自动上传群成员信息功能

--- modules/qunyuanbackup.py
+++ modules/qunyuanbackup.py
@@ -36,14 +36,14 @@ async def diaotu_upload(app: Ariadne, group: Group, member: Member):
         group, MessageChain([Plain("正在获取群成员信息，请稍候...")])
     )
     member_list = await app.get_member_list(group)
+    filename = f"{time.strftime('%Y%m%d-%H:%M:%S',time.localtime())}_{group.id}.csv"
+    await save_members_to_csv(member_list, filename)
+    with open(f"./qunyuanbackup/{filename}.csv", "rb") as f:
+        csv_bytes = f.read()
+    await app.upload_file(csv_bytes, "group", group, name=filename)
     await app.send_group_message(
-        group, MessageChain([Plain(f"群成员信息已获取，共有{len(member_list)}人")])
+        group, MessageChain([Plain("默认自动上传数据到群文件")])
     )
-    await save_members_to_csv(member_list,f"{time.strftime('%Y%m%d-%H:%M:%S',time.localtime())}_{group.id}")
-    await app.send_group_message(group, MessageChain([Plain("群成员信息已保存")]))
-    # await app.send_group_message(
-    #     group, MessageChain([Plain("默认自动上传数据到群文件")])
-    # )
 
 
 async def save_members_to_csv(data, filename):
@@ -70,7 +70,7 @@ async def save_members_to_csv(data, filename):
         parsed_data.append(member_data)
 
     # 异步写入CSV
-    filename = f"./qunyuanbackup/{filename}.csv"
+    filename = f"./qunyuanbackup/{filename}"
     async with aiofiles.open(filename, mode="w", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=parsed_data[0].keys())
         await writer.writeheader()
