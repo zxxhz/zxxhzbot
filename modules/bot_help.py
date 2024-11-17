5bfc1509987fb1ff40c516cc10af4fab164cb298
zxxhz <ou0897@foxmail.com>
2024-11-17 10:56:22 +0800

    bot帮助菜单

---
+++ modules/bot_help.py
@@ -0,0 +1,22 @@
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.message.element import Plain
+from graia.ariadne.message.parser.base import DetectPrefix
+from graia.ariadne.model import Group, Member
+from graia.saya import Channel
+from graia.saya.builtins.broadcast import ListenerSchema
+
+channel = Channel.current()
+
+
+@channel.use(
+    ListenerSchema(
+        listening_events=[GroupMessage], decorators=[DetectPrefix("bot帮助")]
+    )
+)
+async def print_bot_help(app: Ariadne, group: Group, member: Member):
+    text = "zxbot命令菜单\n草图帮助\nping"
+    if member.id == 1582891850:
+        text += "\ncf优选ip\n备份群成员列表"
+    await app.send_message(group, MessageChain(Plain(text)))
