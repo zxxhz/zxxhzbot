fa760d7d1c401e97dc4b2912c1b37b829d9a7d12
zxxhz <ou0897@foxmail.com>
2024-12-07 19:43:04 +0800

    修改屎山

--- modules/bot_help.py
+++ modules/bot_help.py
@@ -16,9 +16,33 @@ channel = Channel.current()
     )
 )
 async def print_bot_help(app: Ariadne, group: Group, member: Member):
-    text = (
-        "zxbot命令菜单\n草图帮助\n网站测速 <域名>\nping <域名>\n百度搜索 <搜索的内容>"
-    )
+    """显示bot的帮助信息
+    
+    Args:
+        app (Ariadne): Ariadne实例
+        group (Group): 群组对象
+        member (Member): 发送命令的成员
+    """
+    # 基础命令列表
+    commands = [
+        "zxbot命令菜单",
+        "草图帮助",
+        "网站测速 <域名>",
+        "ping <域名>", 
+        "百度搜索 <搜索的内容>"
+    ]
+    
+    # 管理员命令列表
+    admin_commands = [
+        "cf优选ip",
+        "备份群成员列表"
+    ]
+    
+    # 拼接基础命令
+    text = "\n".join(commands)
+    
+    # 如果是管理员,添加管理员命令
     if member.id == 1582891850:
-        text += "\ncf优选ip\n备份群成员列表"
+        text += "\n" + "\n".join(admin_commands)
+        
     await app.send_message(group, MessageChain(Plain(text)))
